f252718dad36072fb34d2f7b2640c679814e2df3
hyunvely8 <81628521+hyunvely8@users.noreply.github.com>
2021-11-07 22:56:57 +0900

    Create 3745.cpp

---
+++ algos_assignment/2nd_grade/김채연_chaeyeon20/week8/3745.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+vector<int> lis;
+int main() {
+    int N;
+    while (scanf("%d", &N) != EOF) {
+        lis.clear();
+        for (int i = 0; i < N; i++) {
+            int tmp, ans;
+            scanf("%d", &tmp);
+            if (lis.empty() || lis[lis.size() - 1] < tmp) lis.push_back(tmp);
+            else {
+                int L = 0, R = lis.size() - 1;
+                while (L <= R) {
+                    int mid = (L + R) / 2;
+                    if (lis[mid] >= tmp) {
+                        ans = mid;
+                        R = mid - 1;
+                    }
+                    else L = mid + 1;
+                }
+                lis[ans] = tmp;
+            }
+        }
+        printf("%d\n", lis.size());
+    }
+    return 0;
+}
+
+
+출처: https://jklh0202.tistory.com/entry/BOJ백준-3745-오름세 [10초안에 이해되는]
